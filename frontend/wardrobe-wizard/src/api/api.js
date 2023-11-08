bcc1e1257841e40fde9b7cc560a4d139e546e35e
zhenzhang22 <zhenzh@seas.upenn.edu>
2023-11-07 21:02:58 -0600

    Cloud finished; Upload photo connected, and still need to fix type and activity

--- frontend/wardrobe-wizard/src/api/api.js
+++ frontend/wardrobe-wizard/src/api/api.js
@@ -117,3 +117,71 @@ export const randomGenerateClothByTypeEvent = async (type, event) => {
     return []
   }
 }
+
+export const createImage = async (request) => {
+  console.log("CreateImage request",request)
+  console.log("CreateImage URL",request.imageUrl)
+  console.log("CreateImage brand_names",request.brand_names)
+  console.log("CreateImage descriptions",request.descriptions)
+  console.log("CreateImage event",request.event)
+  console.log("CreateImage type",request.type)
+  try {
+    // const response = await axios.post(
+    //   '/uploadImage',
+    //   `imageUrl=${request.imageUrl}&brand_names=${request.brand_names}&descriptions=${request.descriptions}&event=${request.event}&type=${request.type}`,
+    // );
+    const response = await axios.post(
+      '/uploadImage',
+      {
+        imageUrl: request.imageUrl,
+        brand_names: request.brand_names,
+        descriptions: request.descriptions,
+        event: request.event,
+        type: request.type
+      },
+    );
+    return response;
+  } 
+  catch (err) {
+    console.log("The creatImage is breaking")
+    console.log(err.response.data)
+    return err;
+  }
+};
+
+const api_key = "554946298277143"
+const cloud_name = "dldiferrn"
+export const uploadPhotoAPI = async (imageFile) => {
+  let imageUrl = '';
+  const signatureResponse = await axios.get("/get-signature")
+  const data = new FormData()
+  data.append("file", imageFile)
+  data.append("api_key", api_key)
+  data.append("signature", signatureResponse.data.signature)
+  data.append("timestamp", signatureResponse.data.timestamp)
+  const cloudinaryResponse = await axios.post(`https://api.cloudinary.com/v1_1/${cloud_name}/auto/upload`, data, {
+    headers: { "Content-Type": "multipart/form-data" },
+    onUploadProgress: function (e) {
+      console.log(e.loaded / e.total)
+    }
+  })
+  console.log("URL",cloudinaryResponse.data.secure_url)
+
+
+  imageUrl = cloudinaryResponse.data.secure_url
+  let brand_names = "Self-taken"
+  let descriptions = "A cloth taken"
+  let event = "DOG"
+  let type = "DOG"
+  const dbData = {
+    imageUrl,
+    brand_names,
+    descriptions,
+    event,
+    type,
+  };
+  console.log("dbData printout",dbData)
+  console.log("url",dbData.imageUrl)
+  createImage(dbData)
+
+};
