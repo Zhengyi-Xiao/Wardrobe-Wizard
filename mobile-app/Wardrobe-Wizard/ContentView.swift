8557fef3403bb5a8e69e2282a0a7b12eb393135f
zxiao <zxiao@fandm.edu>
2023-11-19 00:07:57 -0500

    update swift

--- mobile-app/Wardrobe-Wizard/ContentView.swift
+++ mobile-app/Wardrobe-Wizard/ContentView.swift
@@ -11,11 +11,13 @@ struct ContentView: View {
     var body: some View {
         if #available(iOS 16.0, *) {
             NavigationStack {
-                WebView(url: URL(string: "https://baidu.com")!)
+                WebView(url: URL(string: "http://52.87.179.156:3000")!)
+                    .ignoresSafeArea()
             }
         } else {
             Text("Sorry, this feature requires iOS 16.0 or higher.")
                 .foregroundColor(.red)
+                .ignoresSafeArea()
         }
     }
 }
